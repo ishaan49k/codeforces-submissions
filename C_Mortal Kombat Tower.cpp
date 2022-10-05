90d33774312f5ef44b0faf26daeff6469d3ef0f4
Ishaan Kulkarni <74403067+ishaan49k@users.noreply.github.com>
2022-10-05 07:10:04 +0000

    Update C_Mortal Kombat Tower.cpp

--- C_Mortal Kombat Tower.cpp
+++ C_Mortal Kombat Tower.cpp
@@ -135,10 +135,9 @@ void solve(){
     dp[1][0] = a[0] + a[1];
     dp[1][1] = a[0];
     for(int i=2; i<n; i++){
-        dp[i][0] = min(dp[i][0], dp[i-2][1] + a[i-1] + a[i]);
-        dp[i][0] = min(dp[i][0], dp[i-1][1] + a[i]);
-        dp[i][1] = min(dp[i][1], dp[i-2][0]);
-        dp[i][1] = min(dp[i][1], dp[i-1][0]);	
+        dp[i][0] = min(dp[i-1][1] + a[i], dp[i-2][1] + a[i-1] + a[i]);
+        dp[i][1] = min(dp[i-1][0], dp[i-2][0]);
+ 
     }
 
     cout << min(dp[n-1][0], dp[n-1][1]) << endl;
